6f5fae265d3eaf92a3e2202fccfa7bda7b4ff1c4
Ken Mueller <kenmueller0@gmail.com>
2023-03-19 15:46:44 -0700

    removed keepalive pong timeout so app can be used in background

--- socket/keepAlive.ts
+++ socket/keepAlive.ts
@@ -1,20 +1,11 @@
 import WebSocket from 'ws'
 
-import close from './close'
-
 const PING_INTERVAL = 5000
 
 const keepAlive = (socket: WebSocket) => {
-	let alive = true
-
 	const interval = setInterval(() => {
-		alive ? socket.ping() : close(socket)
-		alive = false
+		socket.ping()
 	}, PING_INTERVAL)
-
-	socket.on('pong', () => {
-		alive = true
-	})
 
 	socket.on('close', () => {
 		clearInterval(interval)
